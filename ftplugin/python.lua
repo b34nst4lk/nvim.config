1c046023bf32b69bd99c1cfa5aae4a736400a5ae
b34nst4lk <jsim89@gmail.com>
2022-12-24 18:17:22 +0800

    added ftplugin python

---
+++ ftplugin/python.lua
@@ -0,0 +1,21 @@
+-- keymaps
+vim.keymap.set("n", "<leader>s", 'o__import__("pdb").set_trace()<ESC>')
+
+-- UI
+vim.opt.colorcolumn = {80, 120}
+
+-- On save
+local function DarkerFormat()
+    vim.cmd("!darker %")
+end
+
+function PythonOnWrite()
+    print("hello")
+    DarkerFormat()
+end
+
+local pytask_group = vim.api.nvim_create_augroup("Python", {clear= true})
+vim.api.nvim_create_autocmd(
+    "BufWritePost",
+    {pattern="*.py", command="silent lua PythonOnWrite()", group=pytask_group}
+)
