4efcbb569378c8ed4fc4e55d13124f5dcdf7ceb9
b34nst4lk <jsim89@gmail.com>
2022-12-24 16:24:31 +0800

    added lsp config for lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,13 +3,54 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-	"python",
+	"pyright",
 	"sumneko_lua",
 	"rust_analyzer",
 })
 
 local cmp = require("cmp")
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings
+})
 
 
+lsp.on_attach(function(client, bufnr)
+	local opts = {
+		buffer = bufnr,
+		remap = false,
+	}
+	vim.keymap.set("n", "<C-o>", vim.lsp.buf.definition, opts) 
+	vim.keymap.set("n", "K", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "<C-h>", vim.lsp.buf.signature_help, opts)
+
+end)
+
+lsp.configure("sumneko_lua", {
+	settings = {
+		Lua={
+			diagnostics = {
+				globals = { 'vim' }
+			}
+		}
+	}
+})
+
+lsp.setup()
+
+vim.diagnostic.config({virtual_text=true})
